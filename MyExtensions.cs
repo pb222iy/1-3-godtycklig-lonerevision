9985c093963400b47800d4c19d9fce79c6841755
Peter Blomdahl <pb222iy@student.lnu.se>
2014-09-25 00:24:31 +0200

    Bulk commit

---
+++ MyExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment
+{
+	static class MyExtensions
+	{
+		/// <summary>
+		///		Extension method for int[] to calculate value dispersion
+		/// </summary>
+		/// <returns>difference between smallest and largest values in array</returns>
+		public static int dispersion( this int[] source )
+		{
+			return source.Max() - source.Min();
+		}
+
+		/// <summary>
+		///		Extension method for int[] to calculate median value
+		/// </summary>
+		/// <returns>median value in provided array</returns>
+		public static int median( this int[] source )
+		{
+			// If source is one long, the choice is pretty simple
+			if ( source.Length == 1 )
+				return source[0];
+
+			// Produce a sorted array of our numbers
+			int[] sorted = source.OrderBy( a => a ).ToArray();
+
+			// If array is even length, average middle two
+			if ( ( sorted.Length & 1 ) == 0 )
+			{
+				int mid = sorted.Length / 2;
+
+				return ( sorted[mid] - sorted[mid-1] ) / 2;
+			}
+			// If array is odd length, return middle value
+			else
+			{
+				return sorted[sorted.Length/2 + 1];
+			}
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			do
+			{
+				// Read number of salaries to input
+				int numSalaries = readInt( Assignment.Resource.promptNumberSalaries );
+
+				// Read all the salaries
+				int[] salaries = readSalaries( Assignment.Resource.promptEnterSalary, numSalaries );
+
+				// Report them salaries
+				viewResult( salaries );
+
+			} while ( isContinuing() );
+		}
+
+		/// <summary>
+		///		Read integer value from console
+		/// </summary>
+		/// <param name="prompt">text prompt to accompany input</param>
+		private static int readInt( string prompt )
+		{
+			int value = -1;
+
+			while ( true )
+			{
+				Console.Write( prompt );
+
+				try
+				{
+					value = int.Parse( Console.ReadLine() );
+
+					break;
+				}
+				catch ( FormatException )
+				{
+					viewMessage( Assignment.Resource.errorFormatException, ConsoleColor.Red );
+				}
+				catch ( OverflowException )
+				{
+					viewMessage( Assignment.Resource.errorOverflowException, ConsoleColor.Red );
+				}
+			}
+
+			return value;
+		}
+		
+		/// <summary>
+		///		Read list of salaries from console
+		/// </summary>
+		/// <param name="prompt">text prompt to accompany input</param>
+		/// <param name="count">number of salaries to read</param>
+		/// <returns>unsorted integer array of salaries</returns>
+		private static int[] readSalaries( string prompt, int count )
+		{
+			int[] salaries = new int[count];
+
+			// Read each salary in turn
+			for ( int i = 0; i < count; i++ )
+			{
+				int salary = -1;
+
+				// No one should be allowed to have a negative salary (owing the company money?),
+				// so let's wrap this while thing in a loop to check for that.
+				// 0 is technically an allowed salary, but I wouldn't want to be that guy.
+				while ( true )
+				{
+					salary = readInt( string.Format( prompt, i+1 ) );
+
+					// If salary is not negative, break while loop to assign value
+					if ( salary >= 0 )
+						break;
+
+					viewMessage( Assignment.Resource.errorNegativeSalary, ConsoleColor.Red );
+				}
+
+				salaries[i] = salary;
+			}
+
+			return salaries;
+		}
+
+		/// <summary>
+		///		Report salary statistics to user
+		/// </summary>
+		private static void viewResult( int[] salaries )
+		{
+			string headerLine = new String( '-', 39 );
+
+			// Calculate all our values with our handy dandy aggregates
+			int median = salaries.median();
+			int average = (int) salaries.Average();
+			int dispersion = salaries.dispersion();
+
+			Console.WriteLine();
+			Console.WriteLine( headerLine );
+			
+			Console.WriteLine( Assignment.Resource.formatSalaryStats, Assignment.Resource.statsMedianSalary, median );
+			Console.WriteLine( Assignment.Resource.formatSalaryStats, Assignment.Resource.statsAverageSalary, average );
+			Console.WriteLine( Assignment.Resource.formatSalaryStats, Assignment.Resource.statsSalaryDispersion, dispersion );
+
+			Console.WriteLine( headerLine );
+
+			// Echo back each salary entered to the user, in the order entered
+			for ( int i = 0; i < salaries.Length; i++ )
+			{
+				if ( i > 0 && i % 3 == 0 )
+					Console.WriteLine();
+
+				Console.Write( Assignment.Resource.formatSalaryPrintout, salaries[i] );
+			}
+		}
+
+		/// <summary>
+		///		Query user whether program should continue.
+		/// </summary>
+		private static bool isContinuing()
+		{
+			viewMessage( Assignment.Resource.promptContinue );
+
+			// If next key pressed is esc (key code 27), return false - do not continue
+			bool value = Console.ReadKey().KeyChar != 27;
+
+			// Clear line after query
+			Console.WriteLine();
+
+			return value;
+		}
+
+		/// <summary>
+		///		Standard method to print status messages to the user.
+		/// </summary>
+		private static void viewMessage( string message, ConsoleColor backgroundColor = ConsoleColor.Blue,
+									ConsoleColor foregroundColor = ConsoleColor.White )
+		{
+			Console.WriteLine( "" );
+
+			Console.ForegroundColor = foregroundColor;
+			Console.BackgroundColor = backgroundColor;
+
+			Console.WriteLine( message );
+
+			Console.ResetColor();
+			Console.WriteLine( "" );
+		}
+	}
+}
